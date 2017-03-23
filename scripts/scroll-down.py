68b6af81cc44f4bc8c3109e3907df5b4c35b9de8
chenzhih03 <chenzhihong03@hotmail.com>
2017-03-23 10:49:42 -0700

    scroll down a website

--- scripts/scroll-down.py
+++ scripts/scroll-down.py
@@ -1,3 +1,4 @@
+#import selenium and time to allow browse the webs and delay
 from selenium import webdriver
 import time
 
@@ -9,9 +10,13 @@ def scroll_down(url, scroll_times = 100, sleep_time = 3):
 #'''
 #Scroll down the website so all the content can be shown
 #'''
-    while i <= 100:
+# depend on the waiting time each refresh takes and number of refreshes you need,
+# adjust your waiting time and number of refreshes
+    num_refreshes = 100
+    wait_time = 3
+    while i <= num_refreshes:
         browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(3)
+        time.sleep(wait_time)
 
 if (__name__ == '__main()__'):
     scroll_down(url, scroll_times = 100, sleep_time = 3):
