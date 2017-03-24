bcfe4f4345d1539ba6dbaa575ddc6cdfa9a32434
chenzhih03 <chenzhihong03@hotmail.com>
2017-03-23 19:21:19 -0700

    plot the confusion matrix

---
+++ scripts/plot_confusion_matrix.py
@@ -0,0 +1,32 @@
+import itertools
+import matplotlib.pyplot as plt
+%matplotlib inline
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, fontsize=6)
+    plt.yticks(tick_marks, classes,fontsize=6)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.figure(figsize=(20,20))
+
